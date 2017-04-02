c801a3f82522c63fa1aec3d2e31ddeb074531229
Chi Zeng <chi@chizeng.com>
2017-04-02 13:32:55 -0700

    Remove build artifacts from the repo

--- temporary_build_artifacts/rename-mapping.js
+++
@@ -1,4 +0,0 @@
-goog.provide('cssVocabulary');
-goog.setCssNameMapping(
-{"shown":"a","utilityBar":"b","bodyWithupdatesMissingWarning":"c","button":"d","audioParamText":"e","marker-arrowhead":"f","propertyValueCategoryTitle":"g","updatesMissingWarning":"h","contentArea":"i","hiddenPane":"j","xButton":"k","widgetRowLeft":"l","widgetRowRight":"m","highlightedAudioNode":"n"}
-,'BY_WHOLE');
