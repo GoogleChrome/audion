7c9b3c924cf994a6eea60b42f9a2624fe3d713a7
Chi Zeng <chi@chizeng.com>
2017-04-02 13:29:38 -0700

    Make WAI gracefully handle unexpected channel values
    Sometimes, code may inadvertently pass non-numeric values into the 2nd or 3rd arguments of `connect` or `disconnect`. The Web Audio API gracefully tries to convert to a number (or default to 0 if it cannot), so WAI should mimic that behavior.

---
+++ temporary_build_artifacts/rename-mapping.js
@@ -0,0 +1,4 @@
+goog.provide('cssVocabulary');
+goog.setCssNameMapping(
+{"shown":"a","utilityBar":"b","bodyWithupdatesMissingWarning":"c","button":"d","audioParamText":"e","marker-arrowhead":"f","propertyValueCategoryTitle":"g","updatesMissingWarning":"h","contentArea":"i","hiddenPane":"j","xButton":"k","widgetRowLeft":"l","widgetRowRight":"m","highlightedAudioNode":"n"}
+,'BY_WHOLE');
