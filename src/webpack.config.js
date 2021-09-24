e3d10ed88e89c443ea61d8ab2186edb664f7bba0
Max Vogler <max.vogler@gmail.com>
2021-09-24 12:08:42 +0200

    Debug up build files. (#1)
    * Clean config.
    
    * Log verbose.
    
    * Remove import.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -12,7 +12,6 @@ module.exports = (env, argv) => ({
   },
   devtool: argv.mode === 'development' ? 'eval' : false,
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
   },
   module: {
