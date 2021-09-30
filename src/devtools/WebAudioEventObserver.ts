4603458dd2ca95394246dbead7be81fc1ee28f02
Max Vogler <maxvo@google.com>
2021-09-30 18:17:34 +0200

    Revert WebAudioEventObserver.ts to HEAD.

--- src/devtools/WebAudioEventObserver.ts
+++ src/devtools/WebAudioEventObserver.ts
@@ -25,28 +25,27 @@ export class WebAudioEventObserver extends Observer<Audion.WebAudioEvent> {
         onNext({method, params});
       };
       const onDetach = () => {
-        chrome.debugger.attach({tabId}, debuggerVersion, () => {
-          chrome.debugger.sendCommand({tabId}, Methods.enable);
-        });
-      };
-      const onNavigated = () => {
-        chrome.debugger.sendCommand({tabId}, Methods.enable);
+        // TODO: Show a warning if the DevTools are still open and allow the
+        // user to re-attach manually, e.g. by pressing a button.
+        // See: https://developer.chrome.com/docs/extensions/reference/debugger/#type-DetachReason
       };
 
-      chrome.debugger.attach({tabId}, debuggerVersion, () => {
-        chrome.debugger.sendCommand({tabId}, Methods.enable);
-      });
       chrome.debugger.onDetach.addListener(onDetach);
       chrome.debugger.onEvent.addListener(onEvent);
-      chrome.devtools.network.onNavigated.addListener(onNavigated);
 
       return () => {
         chrome.debugger.onDetach.removeListener(onDetach);
         chrome.debugger.onEvent.removeListener(onEvent);
-        chrome.devtools.network.onNavigated.removeListener(onNavigated);
         chrome.debugger.sendCommand({tabId}, Methods.disable);
         chrome.debugger.detach({tabId}, () => {});
       };
+    });
+  }
+
+  /** Attaches the chrome.debugger to start observing events. */
+  attach() {
+    chrome.debugger.attach({tabId}, debuggerVersion, () => {
+      chrome.debugger.sendCommand({tabId}, Methods.enable);
     });
   }
 }
