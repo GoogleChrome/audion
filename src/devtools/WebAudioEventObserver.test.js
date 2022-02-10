be62db889ea62dbdb9387ddc79edd306341d45f0
Michael "Z" Goddard <z@bocoup.com>
2022-02-10 03:57:40 -0500

    change: add debugger state machine to WebAudioEventObserver

--- src/devtools/WebAudioEventObserver.test.js
+++ src/devtools/WebAudioEventObserver.test.js
@@ -3,7 +3,7 @@
 import {beforeEach, describe, expect, it, jest} from '@jest/globals';
 
 import {chrome} from '../chrome';
-import {Events} from '../chrome/DebuggerWebAudioDomain';
+import {Event} from '../chrome/DebuggerWebAudioDomain';
 
 import {WebAudioEventObserver} from './WebAudioEventObserver';
 
@@ -53,12 +53,19 @@ describe('WebAudioEventObserver', () => {
     if (jest.isMockFunction(chrome.debugger.attach)) {
       /** @type {function} */ (chrome.debugger.attach.mock.calls[0][2])();
     }
+    expect(chrome.debugger.sendCommand).toBeCalledTimes(1);
+    if (jest.isMockFunction(chrome.debugger.sendCommand)) {
+      /** @type {function} */ (chrome.debugger.sendCommand.mock.calls[0][2])();
+    }
     unsubscribe();
     expect(chrome.debugger.detach).toBeCalled();
+    if (jest.isMockFunction(chrome.debugger.sendCommand)) {
+      /** @type {function} */ (chrome.debugger.sendCommand.mock.calls[1][2])();
+    }
+    expect(chrome.debugger.sendCommand).toBeCalledTimes(2);
     if (jest.isMockFunction(chrome.debugger.detach)) {
       /** @type {function} */ (chrome.debugger.detach.mock.calls[0][1])();
     }
-    expect(chrome.debugger.sendCommand).toBeCalledTimes(2);
     expect(chrome.debugger.onDetach.removeListener).toBeCalled();
     expect(chrome.debugger.onEvent.removeListener).toBeCalled();
   });
@@ -85,10 +92,10 @@ describe('WebAudioEventObserver', () => {
     if (jest.isMockFunction(chrome.debugger.onEvent.addListener)) {
       /** @type {function} */ (
         chrome.debugger.onEvent.addListener.mock.calls[0][0]
-      )('tab', Events.contextCreated, contextCreated);
+      )('tab', Event.contextCreated, contextCreated);
     }
     expect(nextMock).toBeCalledWith({
-      method: Events.contextCreated,
+      method: Event.contextCreated,
       params: contextCreated,
     });
   });
