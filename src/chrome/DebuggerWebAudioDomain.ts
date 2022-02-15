f111fe2796773abbd6359a66bfc7e5904f4f3db5
Michael "Z" Goddard <z@bocoup.com>
2022-02-15 16:40:45 -0500

    fixup! fix: give web audio domain enums descriptive names

--- src/chrome/DebuggerWebAudioDomain.ts
+++ src/chrome/DebuggerWebAudioDomain.ts
@@ -1,11 +1,19 @@
+/**
+ * @file
+ * Strings passed to `chrome.debugger.sendCommand` and received from
+ * `chrome.debugger.onEvent` callbacks.
+ */
+
 import {ProtocolMapping} from 'devtools-protocol/types/protocol-mapping';
 
+/** @see https://chromedevtools.github.io/devtools-protocol/tot/WebAudio/#methods */
 export enum WebAudioDebuggerMethod {
   disable = 'WebAudio.disable',
   enable = 'WebAudio.enable',
   getRealtimeData = 'WebAudio.getRealtimeData',
 }
 
+/** @see https://chromedevtools.github.io/devtools-protocol/tot/WebAudio/#events */
 export enum WebAudioDebuggerEvent {
   audioListenerCreated = 'WebAudio.audioListenerCreated',
   audioListenerWillBeDestroyed = 'WebAudio.audioListenerWillBeDestroyed',
@@ -22,5 +30,6 @@ export enum WebAudioDebuggerEvent {
   nodesDisconnected = 'WebAudio.nodesDisconnected',
 }
 
+/** @see https://chromedevtools.github.io/devtools-protocol/tot/WebAudio/#types */
 export type WebAudioDebuggerEventParams<Name extends WebAudioDebuggerEvent> =
   ProtocolMapping.Events[Name];
