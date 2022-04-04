1e093e249b07639035ff0d73ddb697ca25bd1945
Michael "Z" Goddard <z@bocoup.com>
2022-04-04 14:27:33 -0400

    fixup! chore: move layout map function to its own file

--- src/devtools/setOptionsToGraphContext.ts
+++ src/devtools/setOptionsToGraphContext.ts
@@ -8,10 +8,6 @@ export function setOptionsToGraphContext([context, layoutOptions]: [
 ]): Audion.GraphContext {
   if (context.context && context.graph) {
     context.graph.setGraph(layoutOptions);
-    // TODO: dagre's graphlib typings are inaccurate, which is why we use
-    // graphlib's types. Revert to dagre's types once the issue is fixed:
-    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/47439
-    dagre.layout(context.graph as unknown as dagre.graphlib.Graph);
   }
   return context;
 }
