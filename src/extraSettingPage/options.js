b279016a0c949827f27f7df7719bf8ec60bad360
Tenghui Zhang <32180291+TenghuiZhang@users.noreply.github.com>
2023-08-08 11:06:51 -0700

    Fix format

--- src/extraSettingPage/options.js
+++ src/extraSettingPage/options.js
@@ -1,37 +1,46 @@
 /**
- * Initializes the options page by setting up event
- * listeners and restoring saved options.
+ * Initializes the options page by setting up event listeners and
+ * restoring saved options.
  */
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener(
+  'DOMContentLoaded',
   /**
-   * Logs a message to the console
-   * based on whether the checkbox is checked or not.
+   * Handles the DOMContentLoaded event to set up the options page.
    */
-  function saveOptions() {
-    const checkboxValue = document.getElementById('showDebugInfo').checked;
-    /* eslint-disable */
-    chrome.storage.sync.set(
-      {
-        isShownExtraDebugLog: checkboxValue,
-      },
-      function () {
-        console.log('Options saved.');
-      },
-    );
-  }
+  function() {
+    /**
+     * Function to save the options
+     */
+    function saveOptions() {
+      const checkboxValue = document.getElementById('showDebugInfo').checked;
+      /* eslint-disable */
+      chrome.storage.sync.set(
+        {
+          isShownExtraDebugLog: checkboxValue,
+        },
+        function () {
+          console.log('Options saved.');
+        },
+      );
+    }
 
-  // Function to restore the options
-  function restoreOptions() {
-    /* eslint-disable */
-    chrome.storage.sync.get('isShownExtraDebugLog', function (items) {
-      document.getElementById('showDebugInfo').checked =
-        items.isShownExtraDebugLog;
-    });
-  }
+    /**
+     * Function to restore the options
+     */
+    function restoreOptions() {
+      /* eslint-disable */
+      chrome.storage.sync.get('isShownExtraDebugLog', function (items) {
+        document.getElementById('showDebugInfo').checked =
+          items.isShownExtraDebugLog;
+      });
+    }
 
-  // Event listeners
-  document
-    .getElementById('showDebugInfo')
-    .addEventListener('change', saveOptions);
-  restoreOptions();
-});
+    /**
+     * Event listeners
+     */
+    document
+      .getElementById('showDebugInfo')
+      .addEventListener('change', saveOptions);
+    restoreOptions();
+  },
+);
